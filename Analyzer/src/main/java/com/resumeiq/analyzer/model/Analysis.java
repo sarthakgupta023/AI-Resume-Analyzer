8cb2d95a2337537678b56aaf7a949455adab236d
sarthak <sarthakgupta2303@gmail.com>
2026-05-02 16:00:34 +0530

    urladded

--- Analyzer/src/main/java/com/resumeiq/analyzer/model/Analysis.java
+++ Analyzer/src/main/java/com/resumeiq/analyzer/model/Analysis.java
@@ -1,43 +1,5 @@
 package com.resumeiq.analyzer.model;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Document(collection = "analyses")
 public class Analysis {
-
-    @Id
-    private String id;
-
-    private String userEmail;
-
-    private String fileName;
-
-    private String jobDescription;
-
-    private int atsScore;
-
-    private List<String> strengths;
-
-    private List<String> weaknesses;
-
-    private List<String> missingSkills;
-
-    private List<String> suggestions;
-
-    private String summary;
-
-    private LocalDateTime createdAt;
+    
 }
